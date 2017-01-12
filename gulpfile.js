a5d2bd5678defffd81b36a5cdd552f882e2af590
stefanFCB94 <stefan.laeufle@gmail.com>
2017-01-12 22:10:34 +0100

    Complete with unit testing, code coverage and compile scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,64 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const ts = require('gulp-typescript');
+const merge = require('merge2');
+const runSeq = require('run-sequence');
+const istanbul = require('gulp-istanbul');
+const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+const sourcemaps = require('gulp-sourcemaps');
 
-var tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', function() {
-    var tsResult = gulp.src(['src/**/*.ts', '!src/**/*.spec.ts']) // or tsProject.src()
-        .pipe(tsProject());
 
-    return tsResult.js.pipe(gulp.dest('dist'));
+// Compile all Prod and Test-Files
+gulp.task('compile', function() {
+	const tsResult = gulp.src('src/**/*.ts')
+		.pipe(sourcemaps.init())
+		.pipe(tsProject());
+
+	return merge([
+		tsResult.js
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest('dist')),
+		tsResult.dts
+			.pipe(gulp.dest('dist'))
+	]);
+
 });
 
-gulp.task('test', ['scripts'], function() {
-    var tsResult = gulp.src('src/**/*.spec.ts') // or tsProject.src()
-        .pipe(tsProject());
+gulp.task('test:instrument', () => {
+	return gulp.src('./dist/lib/**/*.js')
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire());
+});
 
-    return tsResult.js.pipe(gulp.dest('dist'));
+gulp.task('test:cover', ['test:instrument'], () => {
+	return gulp.src('./dist/test/**/*.js')
+		.pipe(mocha({ reporter: 'spec'}))
+		.pipe(istanbul.writeReports({
+			reporters: [ 'json' ]
+		}))
+		.on('end', remapCoverageFiles);
+});
+
+function remapCoverageFiles() {
+	return gulp.src('./coverage/coverage-final.json')
+	.pipe(remapIstanbul({
+		 reports: {
+			  'html': './coverage/html',
+			  'text-summary': null,
+			  'lcovonly': './coverage/lcov.info'
+		 }
+	}));
+}
+
+gulp.task('test', ['test:cover']);
+
+// Build-Task
+gulp.task('build', function() {
+	runSeq('compile', 'test');
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/**/*.ts', ['test']);
+	gulp.watch('src/**/*.ts', ['build']);
 });
---
+++ src/test/index.spec.ts
@@ -0,0 +1,3 @@
+import { expect } from 'chai';
+import * as mocha from 'mocha';
+
